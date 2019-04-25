fa4c05d2c3d72b301e354c74c9cb36d47af61911
Tadas <thetadas+github@gmail.com>
2019-04-25 22:07:38 +0100

    File Explorer - remove links

--- Win/Configure.ps1
+++ Win/Configure.ps1
@@ -73,11 +73,39 @@ try {
 				ValueType   = "Dword"
 				PsDscRunAsCredential = $CurrentUserCreds
 			}
+
+			# File Explorer - hide various locations from 'This PC'
+			@(
+				'Hide_3D_Objects | {31C0DD25-9439-4F12-BF41-7FF4EDA38722}',
+				'Hide_Music      | {A0C69A99-21C8-4671-8703-7934162FCF1D}',
+				'Hide_Videos     | {35286A68-3C57-41A1-BBB1-0EAE73D76C95}',
+				'Hide_Desktop    | {B4BFCC3A-DB2C-424C-B029-7FE99A87C641}'
+			) | ForEach-Object {
+				$Name, $Guid = $_.Split('|').Trim()
+				Registry $Name {
+					Ensure      = 'Present'
+	
+					Key         = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\$Guid\PropertyBag"
+					ValueName   = 'ThisPCPolicy'
+					ValueData   = 'Hide'
+					ValueType   = 'String'
+				}
+			}
+
+			# File Explorer - hide OneDrive location
+			Registry Hide_OneDrive_Location {
+				Ensure      = 'Present'
+
+				Key         = 'HKEY_LOCAL_MACHINE\Software\Classes\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}'
+				ValueName   = 'System.IsPinnedToNameSpaceTree'
+				ValueData   = '0'
+				ValueType   = 'Dword'
+			}
 		}
 	}
 	MyConfig -OutputPath $MOF_OutputPath -ConfigurationData $ConfigData
 
-	Start-DscConfiguration -Wait -Verbose -Path $MOF_OutputPath
+	Start-DscConfiguration -Wait -Verbose -Path $MOF_OutputPath -Force
 
 } finally {
 	# Clean up the MOF as it contains the credentials. Once applied it's stored encrypted (WMF 5.0)
